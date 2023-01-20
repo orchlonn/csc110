8b51912cd7363958098dd0faad464d08b816960d
orchlonn <orchllnz@gmail.com>
2023-01-20 01:09:32 -0800

    2nd hw added

---
+++ homeworks/hw_2.py
@@ -0,0 +1,13 @@
+import turtle
+
+x = turtle.Turtle()
+x.shape('turtle')
+x.stamp()
+#for i in range(10):
+ #   x.forward(50)
+  #  x.left(360 / 10)
+    
+for i in range(10):
+    x.penup()
+    x.forward(100)
+    x.pendown()
